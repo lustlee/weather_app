62ad78d8f8acae79867a62a93a18d2ec68558931
Ramazan Tokoshev <r.tokoshev@gmail.com>
2025-06-17 13:01:18 +0600

    first commit

---
+++ lib/home_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class WeatherAppHomeScreen extends StatelessWidget {
+  const WeatherAppHomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}

eb1a815a641b91bc4f771a010436152a5b2c91db
Ramazan Tokoshev <r.tokoshev@gmail.com>
2025-06-17 15:20:41 +0600

    create logic for changing theme mode in home screen

--- lib/view/home_screen.dart
+++ lib/view/home_screen.dart
@@ -1,10 +1,58 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:weather_app/provider/theme_provider.dart';
 
-class WeatherAppHomeScreen extends StatelessWidget {
+class WeatherAppHomeScreen extends ConsumerStatefulWidget {
   const WeatherAppHomeScreen({super.key});
 
   @override
+  ConsumerState<WeatherAppHomeScreen> createState() => _WeatherAppHomeScreenState();
+}
+
+class _WeatherAppHomeScreenState extends ConsumerState<WeatherAppHomeScreen> {
+  @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    final theme = Theme.of(context);
+    final themeMode = ref.watch(themeNotifierProvider);
+    final notifier = ref.read(themeNotifierProvider.notifier);
+    final isDark = themeMode == ThemeMode.dark;
+
+    return Scaffold(
+      backgroundColor: theme.primaryColor,
+      appBar: AppBar(
+        backgroundColor: theme.primaryColor,
+        actions: [
+          SizedBox(width: 25),
+          SizedBox(
+            width: 320,
+            height: 50,
+            child: TextField(
+              decoration: InputDecoration(
+                labelText: 'Search City',
+                prefixIcon: Icon(Icons.search, color: theme.colorScheme.surface),
+                labelStyle: TextStyle(color: theme.colorScheme.surface),
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(15),
+                  borderSide: BorderSide(color: theme.colorScheme.surface),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(15),
+                  borderSide: BorderSide(color: theme.colorScheme.surface),
+                ),
+              ),
+            ),
+          ),
+          Spacer(),
+          GestureDetector(
+            onTap: notifier.toggleTheme,
+            child: Icon(
+              isDark ? Icons.light_mode : Icons.dark_mode,
+              color: isDark ? Colors.black : Colors.white,
+            ),
+          ),
+          SizedBox(width: 18),
+        ],
+      ),
+    );
   }
 }
